a79122997b5a3d3a45b9f221b90908e8c4088d32
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2023-02-26 16:06:50 +0100

    Add sample smart contract
    - fix bug with handling previous image

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 
 'use strict';
 
-const test = require('./lib/smart-contracts/resource-manager-test');
+const test = require('./lib/smart-contracts/resource-manager-test.js');
+const flightManager = require('./lib/smart-contracts/flight-booking-manager.js');
 
 module.exports.test = test;
-module.exports.contracts = [ test ];
+module.exports.flightManager = flightManager;
+module.exports.contracts = [ flightManager, test ];
