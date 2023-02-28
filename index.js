10328aefd41f9e6004260672f2f9acbc44b02c30
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2023-02-28 16:59:06 +0100

    separate resource-manager into its own smart contract

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 
 const test = require('./lib/smart-contracts/resource-manager-test.js');
 const flightManager = require('./lib/smart-contracts/flight-booking-manager.js');
-
+const rm = require('./lib/resource-manager/resource-manager.js');
 module.exports.test = test;
 module.exports.flightManager = flightManager;
-module.exports.contracts = [ flightManager, test ];
+module.exports.rm = rm;
+module.exports.contracts = [ flightManager, rm, test ];
